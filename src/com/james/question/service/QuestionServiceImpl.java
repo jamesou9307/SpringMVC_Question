30c29c49dcf125f45971b04d6cd6d986b0968deb
fsuser <291387044@qq.com>
2017-11-06 21:24:28 +0800

    getAllQuestions commit

--- src/com/james/question/service/QuestionServiceImpl.java
+++ src/com/james/question/service/QuestionServiceImpl.java
@@ -8,8 +8,11 @@ import java.util.List;
 @Service
 public class QuestionServiceImpl  implements QuestionService{
 
+    private List<Question> questions;
+
     @Override
     public List<Question> getAllQuestions() {
-        return null;
+        return questions;
     }
+
 }
